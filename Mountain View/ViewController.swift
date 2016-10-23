66b4bdd39961169e6e90bcee6b5c5906089a5542
andrewjkim <ajkim@berkeley.edu>
2016-10-22 21:34:59 -0700

    Added functionality to calculate percentages -- need to figure out app permissions now

--- Mountain View/ViewController.swift
+++ Mountain View/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import HealthKit
 
 var MTN_HEIGHT = 8848
+var METERS = 3.6576
 let healthStore: HKHealthStore? = {
     if HKHealthStore.isHealthDataAvailable() {
         return HKHealthStore()
@@ -29,22 +30,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    @IBAction func Click(_ sender: UIButton) {
-        let flightCount = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.flightsClimbed)
-        let typesToRead = NSSet(object: flightCount)
-        healthStore?.requestAuthorization(toShare: nil, read: typesToRead as? Set<HKObjectType>, completion: { Void in
-//            if (success) {
-//                print("SUCCESS")
-//            } else {
-//                print(error)
-//            }
-        })
-        
-        
-        percentage.text = "100%"
+    
+    func completion(flightsClimbed: Double) {
+        let apprheight = Int(flightsClimbed * METERS)
+        let pctge = (apprheight / MTN_HEIGHT) * 100
+        percentage.text = String(pctge) + "%"
     }
     @IBOutlet weak var percentage: UILabel!
+
+    @IBAction func Click(_ sender: UIButton) {
+        let type = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.flightsClimbed)
+        let date = NSDate()
+        let cal = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)!
+        let newDate = cal.startOfDay(for: date as Date)
+        
+        let predicate = HKQuery.predicateForSamples(withStart: newDate, end: NSDate() as Date, options: .strictStartDate)
+        let interval: NSDateComponents = NSDateComponents()
+        interval.day = 1
+        
+        let query = HKStatisticsCollectionQuery(quantityType: type!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: newDate as Date, intervalComponents: interval as DateComponents)
+        
+        query.initialResultsHandler = { query, results, error in
+            if error != nil {
+                //  Something went Wrong
+                return
+            }
+            if let myResults = results{
+                myResults.enumerateStatistics(from: NSDate.distantPast as Date, to: NSDate.distantFuture as Date) {
+                    statistics, stop in
+                    
+                    if let quantity = statistics.sumQuantity() {
+                        
+                        let flights = quantity.doubleValue(for: HKUnit.count())
+                        
+                        self.completion(flightsClimbed: flights)
+                        
+                    }
+                }
+            }
+        }
+        healthStore?.execute(query)
+    }
 }
 
